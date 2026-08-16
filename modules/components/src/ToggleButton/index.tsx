d612e1a2eb38f1a46085b5fe5af5dc35c1031a63
biolad <biolad@example.com>
2026-08-15 18:56:04 -0700

    basic ToggleButton refactor

--- modules/components/src/ToggleButton/index.tsx
+++ modules/components/src/ToggleButton/index.tsx
@@ -1,98 +1,46 @@
 import cx from 'classnames';
 
-import Button from '#Button/index';
-import { useThemeContext } from '#ThemeContext/index';
 import noopFn, { emptyObj } from '#utils/noops';
 
-import type Props from './types';
+import { NonEmptyArray } from '#types/utility.js';
+import { FC, HTMLProps } from 'react';
 
-// TODO: temprorarily quieting down TS errors to help migration
+export interface Option {
+	disabled?: boolean;
+	title?: FC<{ toggleStatus?: string } | undefined>;
+	value: string;
+}
+
+interface Props extends Omit<HTMLProps<HTMLButtonElement>, 'onChange'> {
+	onChange?: ({ value }: { value: string }) => void;
+	options: NonEmptyArray<Option>;
+	value: string;
+}
+
 /**
- * @param {*} props
+ * ToggleButton
+ * @param param0
+ * @returns
  */
-const ToggleButton = ({
-	className,
-	onChange = noopFn,
-	options = [],
-	value: selectedValue = '',
-	theme: {
-		activeBackground: customActiveBackground,
-		activeBorderColor: customActiveBorderColor,
-		activeFontColor: customActiveFontColor,
-		activeFontSize: customActiveFontSize,
-		background: customBackground,
-		borderRadius: customBorderRadius,
-		borderColor: customBorderColor,
-		className: customClassName,
-		style: customCSS,
-		disabledBackground: customDisabledBackground,
-		disabledBorderColor: customDisabledBorderColor,
-		disabledFontColor: customDisabledFontColor,
-		disabledFontSize: customDisabledFontSize,
-		fontColor: customFontColor,
-		fontSize: customFontSize,
-		OptionCSS: customOptionCSS,
-	} = emptyObj,
-}: Props) => {
-	const {
-		colors,
-		components: {
-			Aggregations: {
-				ToggleButton: {
-					activeBackground: themeActiveBackground = colors?.grey?.[200],
-					activeBorderColor: themeActiveBorderColor,
-					activeFontColor: themeActiveFontColor,
-					activeFontSize: themeActiveFontSize,
-					background: themeBackground = colors?.grey?.[50],
-					borderRadius: themeBorderRadius = '0.9rem 50%',
-					borderColor: themeBorderColor = colors?.grey?.[600],
-					className: themeClassName = '',
-					style: themeCSS,
-					disabledBackground: themeDisabledBackground = colors?.grey?.[200],
-					disabledBorderColor: themeDisabledBorderColor,
-					disabledFontColor: themeDisabledFontColor = colors?.grey?.[700],
-					disabledFontSize: themeDisabledFontSize,
-					fontColor: themeFontColor,
-					fontSize: themeFontSize = '0.9rem',
-					OptionCSS: themeOptionCSS,
-				} = emptyObj,
-			} = emptyObj,
-		} = emptyObj,
-	} = useThemeContext({ callerName: 'ToggleButton' });
-
+const ToggleButton = ({ onChange = noopFn, options, value: selectedValue = '', className }: Props) => {
 	return (
-		<div
-			className={cx('toggle-button', className, customClassName, themeClassName)}
-			style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', height: `calc(${themeFontSize} * 2)`, ...themeCSS, ...customCSS }}
-		>
+		<div className={cx('toggle-button', className)}>
 			{options.map(({ disabled = false, title, value = '' }, index) => {
 				const active = selectedValue === value;
 				const clickHandler = () => (disabled ? null : onChange({ value }));
 
 				return (
-					// TODO: restore active/disabled/border-radius pseudo-selector styles via CSS
-					<Button
+					<button
 						className={cx('toggle-button-option', {
 							active,
 							disabled,
 						})}
-						style={{
-							background: customBackground || themeBackground,
-							border: '0.1rem solid',
-							borderColor: customBorderColor || themeBorderColor,
-							color: customFontColor || themeFontColor,
-							flex: 1,
-							fontSize: customFontSize || themeFontSize,
-							padding: '5px',
-							...themeOptionCSS,
-							...customOptionCSS,
-						}}
 						disabled={disabled}
 						key={value || `undefined-${index}`}
 						onClick={clickHandler}
 					>
 						{typeof title === 'function' ? title({ toggleStatus: cx({ active, disabled }) }) : title}
-					</Button>
+					</button>
 				);
 			})}
 		</div>
